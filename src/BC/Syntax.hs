5cd03d646e1255cdb8cdb4a147907be28d33e1f3
Ryan Fanchiotti <ryan@rur.com>
2025-07-12 20:07:03 -0400

    clarified variable name type in syntax

--- src/BC/Syntax.hs
+++ src/BC/Syntax.hs
@@ -4,10 +4,12 @@ module BC.Syntax (
     Definition(..)
 ) where
 
+type VarName = String
+
 -- Essentially an RValue, will need to be checked for LValues in
 -- assignment, increment, decrement, address at a later stage
 data Expr
-    = Var String -- ^ Variable name
+    = Var VarName -- ^ Variable name
 
     -- Basic primitive types
     | IntT Int
@@ -63,9 +65,9 @@ data Expr
 -- Some weird constructs such as chained declarations have been taken out
 -- (ex: auto a; b is not treated like one statement here; this is only relevant for one line while loops and other silly things)
 data Statement
-    = Auto [(String, Maybe Expr)]
-    | Extern [String]
-    | LabelDec String -- ex: L2:
+    = Auto [(VarName, Maybe Expr)]
+    | Extern [VarName]
+    | LabelDec VarName -- ex: L2:
     -- Note that case isn't chained to another statement on purpose because switches without compound statements are usually not intended
     -- This should also always have a const-expr according to the spec
     | Case Expr
@@ -74,14 +76,14 @@ data Statement
     | IfElse Expr Statement Statement
     | While Expr Statement
     | Switch Expr Statement
-    | Goto String
+    | Goto VarName
     | Return (Maybe Expr)
     | ExprT Expr
     deriving (Eq, Ord, Show)
 
 -- A top level definition
 data Definition
-    = Func String [String] Statement -- Function def: name, args, statement
-    | Global String (Maybe Expr) -- Name, init-val
-    | GlobalVec String (Maybe Expr) (Maybe [Expr]) -- Name, size, init-list
+    = Func VarName [VarName] Statement -- Function def: name, args, statement
+    | Global VarName (Maybe Expr) -- Name, init-val
+    | GlobalVec VarName (Maybe Expr) (Maybe [Expr]) -- Name, size, init-list
     deriving (Eq, Show, Ord)
