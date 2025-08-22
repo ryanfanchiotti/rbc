79a90e561a878eb30dce669681563f650c253931
Ryan Fanchiotti <ryan@rur.com>
2025-08-22 16:36:14 -0400

    executable wip

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,23 +1,26 @@
+{-# LANGUAGE DataKinds #-}
+
 module Main where
 
 import qualified BC.Parser as P
-import BC.Analysis
-import qualified BC.CodeGen.AMD64 as A
+import qualified BC.Analysis as A
+import qualified BC.CodeGen.AMD64 as CGA
 import Text.Megaparsec
-import System.Environment
+import Options.Declarative
+import Control.Monad.IO.Class (liftIO)
+
+compile :: Flag "o" '["output-file"] "\"a.out\"" "output location" (Def "a.out" String)
+        -> Flag "S" '["emit-asm"] "false" "stop after assembly stage" Bool
+        -> Flag "c" '["emit-obj"] "false" "stop after object stage" Bool
+        -> Flag "s" '["statically-link"] "false" "use static linking instead of dynamic linking" Bool
+        -> Arg "input-file" String
+        -> Cmd "Ryan's B Compiler" ()
+compile output emit_asm emit_obj static input = do
+    liftIO . putStrLn $ "output = " ++ get output 
+    liftIO . putStrLn $ "emit_asm = " ++ (show $ get emit_asm)
+    liftIO . putStrLn $ "emit_obj = " ++ (show $ get emit_obj)
+    liftIO . putStrLn $ "static = " ++ (show $ get static)
+    liftIO . putStrLn $ "input = " ++ get input
 
 main :: IO ()
-main = do
-     args <- getArgs
-     (input, output) <- case args of
-                    (f:o:_) -> return (f, o)
-                    _ -> error "Input and output file not provided"
-     content <- readFile input
-     prog <- case parse P.pProg input content of 
-          Left bundle -> do
-               error (errorBundlePretty bundle)
-          Right result -> do
-               return result
-     let prog_lines = A.emitProg prog
-     writeFile output (unlines prog_lines)
-
+main = run_ compile
