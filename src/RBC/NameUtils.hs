8836c7791b16d4ae5b6e99d98757c5a30b0eb887
Ryan Fanchiotti <ryan@rur.com>
2025-08-22 22:32:52 -0400

    clearer readme

---
+++ src/RBC/NameUtils.hs
@@ -0,0 +1,21 @@
+module RBC.NameUtils (
+    makeName,
+    makeLabel,
+    makeNameState,
+    NameState
+) where
+
+-- Names are non negative integers that count upwards
+type NameState = Int
+
+makeNameState :: NameState
+makeNameState = 0
+
+-- Names in programs cannot start with an underscore, therefore
+-- all generated names that do are valid 
+makeName :: NameState -> (String, NameState)
+makeName ns = ("_N" ++ (show ns), ns + 1)
+
+makeLabel :: NameState -> (String, NameState)
+makeLabel ns = (".L" ++ (show ns), ns + 1)
+
